f08da44df7b77e7f3152f4c241492d6736a2c073
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-10-10 10:56:14 +0530

    Added running sum of 1d array

---
+++ java/test.java
@@ -0,0 +1,13 @@
+public static void main(String[]args){
+        HashMap<Int, String> map = new HashMap<Int, String>(){
+        1, "I",
+        5, "V",
+        10, "X",
+        50, "L",
+        100, "C",
+        500, "D",
+        1000, "M"
+        };
+
+        System.out.println(map.get(5));
+}
