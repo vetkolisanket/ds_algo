69dfedb8b5ff243ed3c1dcda42028a3d031f78ca
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-10-11 12:40:46 +0530

    Optimised memory usage of running sum of 1 d aaray

--- java/arrays/running_sum_of_1d_array.java
+++ java/arrays/running_sum_of_1d_array.java
@@ -30,11 +30,9 @@ Constraints:
 class Solution {
     public int[] runningSum(int[] nums) {
         int size = nums.length;
-        int arr[] = new int[size];
-        arr[0] = nums[0];
         for(int i = 1; i < size; i++) {
-            arr[i] = arr[i-1] + nums[i];
+            nums[i] = nums[i-1] + nums[i];
         }
-        return arr;
+        return nums;
     }
 }
