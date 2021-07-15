3d4b4dd670eed8901ec9664746858294ef9fcc01
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-07-15 13:40:58 +0530

    Practised string permutation

--- java/strings/general/string_permutations.java
+++ java/strings/general/string_permutations.java
@@ -5,16 +5,27 @@ class StringPermutations{
 		if(args.length > 0) {
 			s = args[0];
 		}
-		stringPermutation(s,"");
+		stringPermutations(s,"");
 	}
 
-	private static void stringPermutation(String s, String prefix){
+	private static void stringPermutations(String s, String prefix){
+		if(s.length()==0){
+			System.out.print(prefix + " ");
+		} else {
+			for(int i=0;i<s.length();i++) {
+				String rem = s.substring(0,i) + s.substring(i+1);
+				stringPermutations(rem, prefix+s.charAt(i));
+			}
+		}
+	}
+
+	private static void stringPermutation2(String s, String prefix){
 		if(s.length()==0){
 			System.out.println(prefix);
 		} else {
 			for(int i=0;i<s.length();i++){
 				String rem = s.substring(0,i) + s.substring(i+1);
-				stringPermutation(rem, prefix+s.charAt(i));
+				stringPermutation2(rem, prefix+s.charAt(i));
 			}
 		}
 	}
