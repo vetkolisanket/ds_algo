3c2bd33f532d2b16a45ae167fb0c3728bf7e8c51
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-09-10 19:40:04 +0530

    Added iterative approach to solving palindrome

--- java/linked-list/ctci/palindrome.java
+++ java/linked-list/ctci/palindrome.java
@@ -22,3 +22,28 @@ LinkedListNode reverseList(LinkedListNode l) {
 	}
 	return r;
 }
+
+//Iterative approach
+boolean isPalindrome(LinkedListNode head){
+	LinkedListNode slow = head;
+	LinkedListNode fast = head;
+
+	Stack<Integer> stack = new Stack();
+	
+	while (fast != null && fast.next != null){
+		stack.push(slow.data);
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+
+	if(fast != null){
+		slow = slow.next;
+	}
+
+	while (slow != null){
+		int top = stack.pop();
+		if (top != slow.data) return false;
+		slow = slow.next;
+	}
+	return true;
+}
