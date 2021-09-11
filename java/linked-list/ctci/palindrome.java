1ea4db744f96c2b450e191fee0838a126038d091
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-09-11 20:05:44 +0530

    Added resursive soln for palindrome example

--- java/linked-list/ctci/palindrome.java
+++ java/linked-list/ctci/palindrome.java
@@ -41,9 +41,42 @@ boolean isPalindrome(LinkedListNode head){
 	}
 
 	while (slow != null){
-		int top = stack.pop();
+		int top = stack.pop().intValue();
 		if (top != slow.data) return false;
 		slow = slow.next;
 	}
 	return true;
+}
+
+//Recursive approach
+boolean isPalindrome(LinkedListNode head) {
+	int length = lengthOfList(head);
+	Result p = isPalindromeRecurse(head, length);
+	return p.result;
+}
+
+Result isPalindromeRecurse(LinkedListNode head, int length){
+	if (head == null || length <= 0) {
+		return new Result(head, true);
+	} else if (length == 1) {
+		return new Result(head.next, true);
+	}
+
+	Result res = isPalindromeRecurse(head.next, length - 2);
+
+	if (!res.result || res.node == null) return res;
+
+	res.result = (head.data == res.node.data);
+	res.node = res.node.next;
+
+	return res;
+}
+
+int lengthOfList(LinkedListNode n){
+	int size = 0;
+	while (n != null) {
+		size++;
+		n = n.next;
+	}
+	return size;
 }
