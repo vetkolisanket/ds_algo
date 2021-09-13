bd40db0c8dfed7c0260c218204de662e28c30edb
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-09-13 18:22:48 +0530

    Added another way of doing intersection

--- java/linked-list/ctci/intersection.java
+++ java/linked-list/ctci/intersection.java
@@ -37,6 +37,59 @@ LinkedListNode move(LinkedListNode l, int steps){
 	return l;
 }
 
+//Another O(n+m) soln
+LinkedListNode findIntersection(LinkedListNode list1, LinkedListNode list2){
+	if(list1 == null || list2 == null) return null;
+
+	Result result1 = getTailAndSize(list1);
+	Result result2 = getTailAndSize(list2);
+
+	if (result1.tail != result2.tail) return null;
+
+	LinkedListNode shorter = result1.size < result2.size ? list1 : list2;
+	LinkedListNode longer = result1.size < result2.size ? list2 : list1;
+
+	longer = getKthNode(longer, Math.abs(result1.size - result2.size));
+
+	while (shorter != longer) {
+		shorter = shorter.next;
+		longer = longer.next;
+	}
+
+	return longer;
+}
+
+class Result {
+	public LinkedListNode tail;
+	public int size;
+	
+	public Result(LinkedListNode tail, int size) {
+		this.tail = tail;
+		this.size = size;
+	}
+}
+
+Result getTailAndSize(LinkedListNode list) {
+	if (list == null) return null;
+
+	int size = 1;
+	LinkedListNode current = list;
+	while (current.next != null) {
+		size++;
+		current = current.next;
+	}
+	return new Result(current, size);
+}
+
+LinkedListNode getKthNode(LinkedListNode head, int k) {
+	LinkedListNode current = head;
+	while (k > 0 && current != null){
+		current = current.next;
+		k--;
+	}
+	return current;
+}
+
 //Brute Force O(mn)
 LinkedListNode intersection(LinkedListNode l1, LinkedListNode l2){
 	while (l2 != null){
