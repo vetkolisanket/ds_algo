6fd60b9723c09dddc636c5cec39f85fffde7adc0
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-09-18 21:18:21 +0530

    Changed the order in which nodes are being added so to make testing preorder and postorder traversal easy

--- java/trees/implementation/MyBinarySearchTree.java
+++ java/trees/implementation/MyBinarySearchTree.java
@@ -44,16 +44,16 @@ class MyBinarySearchTree {
 
 	public static void main(String[] args) {
 		MyBinarySearchTree tree = new MyBinarySearchTree();
-		tree.add(1);
 		tree.add(0);
+		tree.add(1);
 		tree.add(2);
 		tree.add(3);
-		tree.add(7);
-		tree.add(9);
-		tree.add(5);
-		tree.add(8);
-		tree.add(6);
 		tree.add(4);
+		tree.add(5);
+		tree.add(6);
+		tree.add(7);
+		tree.add(8);
+		tree.add(9);
 		tree.printInOrderTraversal();
 	}
 
