4eaaf108d4b3aa0b1946ce752d88e1d909a00a34
Sanket Vetkoli <vetkolisanket@gmail.com>
2021-10-06 20:53:51 +0530

    Update find_all_duplicates_in_an_array.java

--- java/arrays/leetcode/find_all_duplicates_in_an_array.java
+++ java/arrays/leetcode/find_all_duplicates_in_an_array.java
@@ -4,9 +4,6 @@
 Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
 
 You must write an algorithm that runs in O(n) time and uses only constant extra space.
-*/
-
- 
 
 Example 1:
 
@@ -28,7 +25,7 @@ n == nums.length
 1 <= n <= 105
 1 <= nums[i] <= n
 Each element in nums appears once or twice.
-
+*/
 
 class Solution {
 	public List<Integer> findDuplicates(int[] nums) {
