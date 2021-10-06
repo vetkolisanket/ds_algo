560758e0d6a863aab581580e35814ffa04db43df
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-10-06 20:51:07 +0530

    Added find first and last position of an element in a sorted array example

---
+++ java/arrays/leetcode/find_first_and_last_position_of_an_element_in_a_sorted_array.java
@@ -0,0 +1,73 @@
+/*
+34. Find First and Last Position of Element in Sorted Array
+
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+*/
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] output = new int[2];
+        output[0] = findFirst(nums, target);
+        output[1] = findLast(nums, target);
+        return output;
+    }
+    
+    private int findFirst(int[] nums, int target) {
+        int index = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start+end)/2;
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            if (nums[mid] == target) index = mid;
+        }
+        return index;
+    }
+    
+    private int findLast(int[] nums, int target) {
+        int index = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start+end)/2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            if (nums[mid] == target) index = mid;
+        }
+        return index;
+    }
+}
