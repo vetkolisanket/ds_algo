0b7a2fb3965ee7af549a1826923eecb230879c20
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-10-10 21:19:57 +0530

    Added a faster subset soln

--- java/graphs/leetcode/backtracking/subsets.java
+++ java/graphs/leetcode/backtracking/subsets.java
@@ -24,6 +24,26 @@ Constraints:
 All the numbers of nums are unique.
 */
 
+//Faster soln
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList();
+        generate(list, new ArrayList(), nums, 0);
+        return list;
+    }
+    
+    private void generate(List<List<Integer>> list, List<Integer> tempList, int[] nums, int i) {
+        if (i == nums.length) {
+            list.add(new ArrayList(tempList));
+            return;
+        }
+        generate(list, tempList, nums, i+1);
+        tempList.add(nums[i]);
+        generate(list, tempList, nums, i+1);
+        tempList.remove(tempList.size() - 1);
+    }
+}
+
 //backtracking soln
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
