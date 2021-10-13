bfe2cdfc811a4655fdc68555843288e663ed80a5
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-10-13 06:41:39 +0530

    Added permutation 2 example

---
+++ java/graphs/leetcode/backtracking/permutation_2.java
@@ -0,0 +1,47 @@
+/*
+47. Permutations II
+
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+Example 2:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 8
+-10 <= nums[i] <= 10
+*/
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList();
+        backtrack(list, new ArrayList(), nums, new boolean[nums.length]);
+        return list;
+    }
+    
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, boolean[] used) {
+        if (tempList.size() == nums.length) list.add(new ArrayList(tempList));
+        else {
+            for (int i = 0; i < nums.length; i++) {
+                if (used[i] || i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+                tempList.add(nums[i]);
+                used[i] = true;
+                backtrack(list, tempList, nums, used);
+                used[i] = false;
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
