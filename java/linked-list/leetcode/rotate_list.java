c4acda7183c29ef8302cff2a3aba79e4bb9a38ee
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-11-14 06:46:08 +0530

    Added rotate list example

---
+++ java/linked-list/leetcode/rotate_list.java
@@ -0,0 +1,71 @@
+/*
+61. Rotate List
+
+Given the head of a linked list, rotate the list to the right by k places.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+Example 2:
+
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        //size of the list
+        int size = getSize(head);
+        if (size == 0) return head;
+        //k mod size
+        k %= size;
+        //2 pointer to kth elem from last
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //last point to head
+        fast.next = head;
+        //head point to slow pointers next
+        head = slow.next;
+        slow.next = null;
+        //return head
+        return head;
+    }
+    
+    private int getSize(ListNode node) {
+        int size = 0;
+        while (node != null) {
+            size++;
+            node = node.next;
+        }
+        return size;
+    }
+}
