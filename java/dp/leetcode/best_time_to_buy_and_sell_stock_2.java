bc0314fa1449a8b6e686d683874d2459de5d1bd0
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-12-18 19:27:20 +0530

    Added a smaller but slower soln to best time to buy and sell stocks 2 example

--- java/dp/leetcode/best_time_to_buy_and_sell_stock_2.java
+++ java/dp/leetcode/best_time_to_buy_and_sell_stock_2.java
@@ -65,3 +65,20 @@ class Solution {
         return i;
     }
 }
+
+//A smaller but slower and a bit more difficult to understand soln
+class Solution {
+    public int maxProfit(int[] A) {
+        int n = A.length, lastBuy = -A[0], lastSold = 0;
+        if (n == 0) return 0;
+                                
+        for (int i = 1; i < n; i++) {
+            int curBuy = Math.max(lastBuy, lastSold - A[i]);
+            int curSold = Math.max(lastSold, lastBuy + A[i]);
+            lastBuy = curBuy;
+            lastSold = curSold;
+        }
+        
+        return lastSold;
+    }
+}
