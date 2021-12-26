76f478dadef322d508db0eabee5b0d4d78e38f0d
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-12-26 23:55:53 +0530

    Added gas station example

--- java/arrays/leetcode/gas_station.java
+++ java/arrays/leetcode/gas_station.java
@@ -48,6 +48,8 @@ cost.length == n
 2, The tank should never be negative, so restart whenever there is a negative number.
 */
 
+//Readable approach
+
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int sumGas = 0, sumCost = 0, tank = 0, start = 0;
@@ -62,5 +64,20 @@ class Solution {
         }
         if (sumGas < sumCost) return -1;
         return start;
+    }
+}
+
+//Another approach which is faster
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0, tank = 0, start = 0;
+        for (int i=0;i<gas.length;i++) {
+            if ((tank = tank+gas[i]-cost[i]) < 0) {
+                start = i+1;
+                total += tank;
+                tank = 0;
+            }
+        }
+        return total+tank < 0 ? -1 : start;
     }
 }
