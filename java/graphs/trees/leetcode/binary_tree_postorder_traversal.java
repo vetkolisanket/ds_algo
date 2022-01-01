b1005a9569b6a03fd656fc0335af071a5d83a7f2
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-01-01 10:11:21 +0530

    Added a faster iterative version of postorder traversal

--- java/graphs/trees/leetcode/binary_tree_postorder_traversal.java
+++ java/graphs/trees/leetcode/binary_tree_postorder_traversal.java
@@ -45,6 +45,24 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
  * }
  */
 
+//Faster iterative soln
+class Solution {
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> list = new LinkedList();
+        if (root == null) return list;
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            list.addFirst(node.val);
+            if (node.left != null) stack.push(node.left);
+            if (node.right != null) stack.push(node.right);
+        }
+        return list;
+    }
+}
+
 //Iterative soln
 class Solution {
     
