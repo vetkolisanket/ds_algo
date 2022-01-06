c971dfeb48ae9f821d7e623ae04df8117745b9b7
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-01-06 07:46:18 +0530

    Added reverse words in a string example

---
+++ java/strings/leetcode/reverse_words_in_a_string.java
@@ -0,0 +1,92 @@
+/*
+151. Reverse Words in a String
+
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+Constraints:
+
+1 <= s.length <= 10^4
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ 
+
+Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+*/
+
+//A more interview appropriate soln which is also faster
+class Solution {
+    public String reverseWords(String s) {
+        char[] a = s.toCharArray();
+        int n = a.length;
+        
+        reverse(a, 0, n-1);
+        reverseWords(a, n);
+        return cleanSpaces(a, n);
+    }
+    
+    private void reverse(char[] a, int i, int n) {
+        int j = n;
+        while (i<j) {
+            char c = a[j];
+            a[j--] = a[i];
+            a[i++] = c;
+        }
+    }
+    
+    private void reverseWords(char[] a, int n) {
+        int i=0,j=0;
+        while (i<n) {
+            while (i<j||i<n && a[i] == ' ') i++;
+            while (j<i||j<n && a[j] != ' ') j++;
+            reverse(a, i, j-1);
+        }
+    }
+    
+    private String cleanSpaces(char[] a, int n) {
+        int i=0,j=0;
+        while (j<n) {
+            while (j<n && a[j] == ' ') j++;
+            while (j<n && a[j] != ' ') a[i++] = a[j++];
+            while (j<n && a[j] == ' ') j++;
+            if (j<n) a[i++] = ' ';
+        }
+        return new String(a).substring(0, i);
+    }
+}
+
+//My soln
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i=arr.length-1;i>0;i--) {
+            sb.append(arr[i]).append(" ");
+        }
+        sb.append(arr[0]);
+        return sb.toString();
+    }
+}
