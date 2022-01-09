8e587c0188c98bbc077afc6c1c2b8a1ff217a1d7
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-01-09 23:56:05 +0530

    Added a better soln for intersection of two linked lists example

--- java/linked-list/leetcode/intersection_of_two_linked_lists.java
+++ java/linked-list/leetcode/intersection_of_two_linked_lists.java
@@ -73,6 +73,22 @@ Follow up: Could you write a solution that runs in O(m + n) time and use only O(
  *     }
  * }
  */
+
+//A better soln
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
+
+//My soln
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         int m = getLength(headA);
