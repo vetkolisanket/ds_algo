5c78878f34e26ebad7be85aec1dea170009474d2
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-01-15 22:04:05 +0530

    Added one more soln which is weird and I didn't understand completely to number of 1 bits example

--- java/bit-manipulation/leetcode/number_of_1_bits.java
+++ java/bit-manipulation/leetcode/number_of_1_bits.java
@@ -55,6 +55,19 @@ public class Solution {
     }
 }
 
+//Another weird soln which I didn't understand
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        n = (n & (0x55555555)) + ((n >> 1) & (0x55555555));
+        n = (n & (0x33333333)) + ((n >> 2) & (0x33333333));
+        n = (n & (0x0f0f0f0f)) + ((n >> 4) & (0x0f0f0f0f));
+        n = (n & (0x00ff00ff)) + ((n >> 8) & (0x00ff00ff));
+        n = (n & (0x0000ffff)) + ((n >> 16) & (0x0000ffff));
+        return n;
+    }
+}
+
 //Better soln
 public class Solution {
     // you need to treat n as an unsigned value
