fbc61be3d1e50748abb64f63e0423d803d954a5f
Sanket Vetkoli <vetkolisanket@gmail.com>
2022-01-23 11:35:56 +0530

    Update rotate_image.java

--- java/arrays/leetcode/rotate_image.java
+++ java/arrays/leetcode/rotate_image.java
@@ -36,6 +36,27 @@ matrix[i].length == n
 
 */
 
+//Another way which feels more intuituve
+class Solution {
+    public void rotate(int[][] matrix) {
+        int s = 0, e = matrix.length-1;
+        while (s < e) {
+            int[] temp = matrix[s];
+            matrix[s] = matrix[e];
+            matrix[e] = temp;
+            s++; e--;
+        }
+        
+        for (int i=0;i<matrix.length;i++) {
+            for (int j=0;j<i;j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
+
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
