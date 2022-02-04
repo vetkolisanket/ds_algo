54d137a96b745da68c4ef9b1732193063409a94a
Sanket Vetkoli <vetkolisanket@gmail.com>
2022-02-04 21:55:22 +0530

    Update minimum_window_substring.java

--- java/strings/leetcode/minimum_window_substring.java
+++ java/strings/leetcode/minimum_window_substring.java
@@ -38,7 +38,42 @@ s and t consist of uppercase and lowercase English letters.
 Follow up: Could you find an algorithm that runs in O(m + n) time?
 */
 
-//O(n^2) soln
+//Another O(m+n) soln which is faster
+class Solution {
+    public String minWindow(String s, String t) {
+        int sLength = s.length();
+        int tLength = t.length();
+        if (sLength < tLength) return "";
+        Map<Character, Integer> map = new HashMap();
+        for (char c : t.toCharArray()) map.put(c, map.getOrDefault(c, 0)+1);
+        int left = 0;
+        int minLeft = 0;
+        int minLength = sLength + 1;
+        int count = 0;
+        for (int right=0;right < sLength; right++) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c)-1);
+                if (map.get(c) >= 0) count++;
+            }
+            while (count == tLength) {
+                if (right-left+1 < minLength) {
+                    minLeft = left;
+                    minLength = right-left+1;
+                }
+                char l = s.charAt(left);
+                if (map.containsKey(l)) {
+                    map.put(l, map.get(l)+1);
+                    if (map.get(l) > 0) count--;
+                }
+                left++;
+            }
+        }
+        return minLength == sLength+1 ? "" : s.substring(minLeft, minLeft + minLength);
+    }
+}
+
+//O(m+n) soln
 class Solution {
     public String minWindow(String s, String t) {
         if (s == null || t == null || s.length() == 0 || t.length() == 0 || s.length() < t.length()) return "";
