2ec69e47499fe9e2787da7dbc4c45b1b518f2685
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-02-14 21:44:30 +0530

    Added minimum platforms example

---
+++ java/searching/gfg/minimum_platforms.java
@@ -0,0 +1,185 @@
+/*
+Minimum Platforms
+
+Given arrival and departure times of all trains that reach a railway station. Find the minimum number of platforms required for the railway station so that no train is kept waiting.
+Consider that all the trains arrive on the same day and leave on the same day. Arrival and departure time can never be the same for a train but we can have arrival time of one train equal to departure time of the other. At any given instance of time, same platform can not be used for both departure of a train and arrival of another train. In such cases, we need different platforms.
+
+
+Example 1:
+
+Input: n = 6 
+arr[] = {0900, 0940, 0950, 1100, 1500, 1800}
+dep[] = {0910, 1200, 1120, 1130, 1900, 2000}
+Output: 3
+Explanation: 
+Minimum 3 platforms are required to 
+safely arrive and depart all trains.
+Example 2:
+
+Input: n = 3
+arr[] = {0900, 1100, 1235}
+dep[] = {1000, 1200, 1240}
+Output: 1
+Explanation: Only 1 platform is required to 
+safely manage the arrival and departure 
+of all trains. 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findPlatform() which takes the array arr[] (denoting the arrival times), array dep[] (denoting the departure times) and the size of the array as inputs and returns the minimum number of platforms required at the railway station such that no train waits.
+
+Note: Time intervals are in the 24-hour format(HHMM) , where the first two characters represent hour (between 00 to 23 ) and the last two characters represent minutes (this may be > 59).
+
+
+Expected Time Complexity: O(nLogn)
+Expected Auxiliary Space: O(n)
+
+
+Constraints:
+1 ≤ n ≤ 50000
+0000 ≤ A[i] ≤ D[i] ≤ 2359
+*/
+
+//My O(nlgn) time O(n) soln
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            
+            int arr[] = new int[n];
+            int dep[] = new int[n];
+            
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+              arr[i] = Integer.parseInt(str[i]);
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                dep[i] = Integer.parseInt(str[i]);
+                
+            System.out.println(new Solution().findPlatform(arr, dep, n));
+        }
+    }
+    
+    
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        Set<Integer> set = new TreeSet();
+        for (int i=0;i<n;i++) {
+            set.add(arr[i]);
+            set.add(dep[i]);
+        }
+        int size = set.size();
+        int[] res = new int[size];
+        List<Integer> list = new ArrayList(set);
+        for (int i=0;i<n;i++) {
+            int start = arr[i];
+            int end = dep[i];
+            int startIdx = list.indexOf(start);
+            int endIdx = list.indexOf(end);
+            for (int j=startIdx;j<=endIdx;j++) {
+                res[j]++;
+            }
+        }
+        int ans = 0;
+        for (int i=0;i<size;i++) {
+            ans = Math.max(ans, res[i]);
+        }
+        return ans;
+    }
+    
+}
+
+//A O(n) time and O(1) soln
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            
+            int arr[] = new int[n];
+            int dep[] = new int[n];
+            
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+              arr[i] = Integer.parseInt(str[i]);
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                dep[i] = Integer.parseInt(str[i]);
+                
+            System.out.println(new Solution().findPlatform(arr, dep, n));
+        }
+    }
+    
+    
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        int[] time = new int[2361];
+        for (int i=0;i<n;i++) {
+            time[arr[i]]++;
+            time[dep[i]+1]--;
+        }
+        for (int i=1;i<2361;i++) {
+            time[i] += time[i-1];
+        }
+        int ans = 0;
+        for (int i=0;i<2361;i++) {
+            ans = Math.max(ans, time[i]);
+        }
+        return ans;
+    }
+    
+}
+
+
