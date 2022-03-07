d9edd9d8c8106906cac19c6ac71a91c1ee826f3e
Sanket Vetkoli <vetkolisanket@gmail.com>
2022-03-07 10:41:25 +0530

    Update maximal_rectangle.java
    Added a soln with explanation

--- java/arrays/leetcode/maximal_rectangle.java
+++ java/arrays/leetcode/maximal_rectangle.java
@@ -38,6 +38,71 @@ matrix[i][j] is '0' or '1'.
 */
 
 class Solution {
+    public int maxArea(int M[][], int n, int m) {
+        // add code here.
+        //Array to track height so far at each index of the row, height[i] == j
+        //implies j contiguous rows have value 1 at index j
+        int[] height = new int[m];
+        //Array to track the starting index for each height index, left[i] == j
+        //implies starting index of height[i] is j
+        int[] left = new int[m];
+        //Array to track the ending position (index+1) of each height index,
+        //right[i] == j implies ending position of height[i] is j
+        int[] right = new int[m];
+        //Populating the right array with m, this is to imply the ending index of
+        //any height is m
+        Arrays.fill(right, m);
+        int maxArea = 0;
+        for (int i=0;i<n;i++) {
+            //Variable to store the starting index of current height
+            int curLeft = 0 
+            //Variable to store the ending index of current height
+            int curRight = m;
+            for (int j=0;j<m;j++) {
+                //If current cell is 1, increment the height
+                if (M[i][j] == 1) height[j]++;
+                //Else reset the height to 0
+                else height[j] = 0;
+            }
+            
+            for (int j=0;j<m;j++) {
+                //If current cell is 1, starting index for height[j] will be 
+                //max of previous rows starting index for column j or current
+                //rows starting index for height[j]
+                if (M[i][j] == 1) left[j] = Math.max(left[j], curLeft);
+                else {
+                    //else resetting left[j] to 0 for next rows computation
+                    left[j] = 0;
+                    //current cell was 0, so maybe the next column could contain
+                    //1 hence updating curLeft to next column i.e. j+1
+                    curLeft = j+1;
+                }
+            }
+            
+            for (int j=m-1;j>=0;j--) {
+                //If current cell is 1, ending index for height[j] will be min of
+                //previous rows ending index for column j or current rows ending
+                //index for height[j]
+                if (M[i][j] == 1) right[j] = Math.min(right[j], curRight);
+                else {
+                    //else resetting right[j] to m for next rows computation
+                    right[j] = m;
+                    //current cell was 0, so maybe the next position(index+1)
+                    //could contain 1 hence updating curRight to j
+                    curRight = j;
+                }
+            }
+            
+            for (int j=0;j<m;j++) {
+                //right[j]-left[j] gives the no. of cols having height[j] so far
+                maxArea = Math.max(maxArea, (right[j] - left[j])*height[j]);
+            }
+        }
+        return maxArea;
+    }
+}
+
+class Solution {
     public int maximalRectangle(char[][] matrix) {
         if (matrix == null || matrix.length == 0) return 0;
         int m = matrix.length;
