78ea7e4c6fd9c5d5da404234b3cbf80b7dc661a6
Sanket Vetkoli <vetkolisanket@gmail.com>
2022-04-18 07:13:49 +0530

    Update steps_by_knight.java

--- java/graphs/gfg/steps_by_knight.java
+++ java/graphs/gfg/steps_by_knight.java
@@ -40,6 +40,78 @@ Constraints:
 1 <= Knight_pos(X, Y), Targer_pos(X, Y) <= N
 */
 
+//Slight change in soln to make it better/faster
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int N = Integer.parseInt(br.readLine().trim());
+            String[] S1 = br.readLine().trim().split(" ");
+            String[] S2 = br.readLine().trim().split(" ");
+            int[] KnightPos = new int[2];
+            int[] TargetPos = new int[2];
+            for(int i = 0; i < 2; i++){
+                KnightPos[i] = Integer.parseInt(S1[i]);
+                TargetPos[i] = Integer.parseInt(S2[i]);
+            }
+            Solution obj = new Solution();
+            int ans = obj.minStepToReachTarget(KnightPos, TargetPos, N);
+            System.out.println(ans);
+       }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find out minimum steps Knight needs to reach target position.
+    public int minStepToReachTarget(int KnightPos[], int TargetPos[], int N)
+    {
+        // Code here
+        if (KnightPos[0] == TargetPos[0] && KnightPos[1] == TargetPos[1]) {
+            return 0;
+        }
+        boolean[][] visited = new boolean[N+1][N+1];
+        Queue<int[]> queue = new ArrayDeque();
+        int minSteps = 1;
+        visited[KnightPos[0]][KnightPos[1]] = true;
+        queue.offer(KnightPos);
+        int[][] steps = {{-1,-2}, {-2,-1}, {-2,1}, {-1,2}, {1,2}, {2,1}, {2,-1}, 
+            {1,-2}
+        };
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                int[] pos = queue.poll();
+                for (int[] step: steps) {
+                    int dx = pos[0] + step[0];
+                    int dy = pos[1] + step[1];
+                    if (dx > 0 && dx <= N && dy > 0 && dy <= N && 
+                    !visited[dx][dy]) {
+                        if (dx == TargetPos[0] && dy == TargetPos[1]) {
+                            return minSteps;
+                        }
+                        visited[dx][dy] = true;
+                        queue.offer(new int[]{dx, dy});
+                    }
+                }
+                size--;
+            }
+            minSteps++;
+        }
+        return -1;
+    }
+}
+
 class Solution
 {
     //Function to find out minimum steps Knight needs to reach target position.
