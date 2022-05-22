0f00472db8bd23c89eabc06e7e5c7dca656a4588
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-05-22 12:36:06 +0530

    Added more solns for palindromic substring example

--- java/strings/leetcode/palindromic_substring.java
+++ java/strings/leetcode/palindromic_substring.java
@@ -27,7 +27,82 @@ Constraints:
 s consists of lowercase English letters.
 */
 
-//My soln
+//O(n^2) time expand around possible centers O(1) time
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        int n = s.length();
+        for (int i=0;i<n;i++) {
+            count += countPalindromes(s, i, i);
+            count += countPalindromes(s, i, i+1);
+        }
+        return count;
+    }
+    
+    private int countPalindromes(String s, int low, int high) {
+        int count = 0;
+        while (low >= 0 && high < s.length()) {
+            if (s.charAt(low) != s.charAt(high)) {
+                break;
+            }
+            low--;
+            high++;
+            count++;
+        }
+        return count;
+    }
+}
+
+//O(n^2) soln using dp O(n^2) space
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int ans = 0;
+        for (int i=0;i<n;i++, ans++) {
+            dp[i][i] = true;
+        }
+        for (int i=1;i<n;i++) {
+            dp[i-1][i] = (s.charAt(i-1) == s.charAt(i));
+            if (dp[i-1][i]) ans++;
+        }
+        for (int len=3;len<=n;len++) {
+            for (int i=0,j=i+len-1;j<n;i++,j++) {
+                dp[i][j] = dp[i+1][j-1] && (s.charAt(i) == s.charAt(j));
+                if (dp[i][j]) ans++;
+            }
+        }
+        return ans;
+    }
+}
+
+//Both of the below soln are O(n^3) complexity
+//Another soln which doesn't create additional substrings O(n^3) time and O(1) space
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        int n = s.length();
+        for (int i=0;i<n;i++) {
+            for (int j=i;j<n;j++) {
+                if (isPalindrome(s, i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    private boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//My soln O(n^3) time 
 class Solution {
     public int countSubstrings(String s) {
         int n = s.length();
