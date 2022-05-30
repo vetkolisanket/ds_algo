5fada8a936dd40c43d930215cae57f288665317c
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-05-30 13:07:31 +0530

    Added 3 sum closer example

---
+++ java/arrays/leetcode/3_sum_smaller.java
@@ -0,0 +1,52 @@
+/*
+259. 3Sum Smaller
+
+Given an array of n integers nums and an integer target, find the number of index triplets i, j, k with 0 <= i < j < k < n that satisfy the condition nums[i] + nums[j] + nums[k] < target.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,0,1,3], target = 2
+Output: 2
+Explanation: Because there are two triplets which sums are less than 2:
+[-2,0,1]
+[-2,0,3]
+Example 2:
+
+Input: nums = [], target = 0
+Output: 0
+Example 3:
+
+Input: nums = [0], target = 0
+Output: 0
+ 
+
+Constraints:
+
+n == nums.length
+0 <= n <= 3500
+-100 <= nums[i] <= 100
+-100 <= target <= 100
+*/
+
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        int n = nums.length;
+        int ans = 0;
+        Arrays.sort(nums);
+        for (int i=0;i<n-2;i++) {
+            int j=i+1, k=n-1;
+            while (j < k) {
+                int sum = nums[i]+nums[j]+nums[k];
+                if (sum < target) {
+                    ans += k-j;
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+}
