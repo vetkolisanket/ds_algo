ec4f41afd34a39fdaf9d770a0b3a451fffa7dd50
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-06-30 12:05:05 +0530

    Added minimum moves to equal array elements 2 example

---
+++ java/arrays/leetcode/minimum_moves_to_equal_array_elements_2.java
@@ -0,0 +1,72 @@
+/*
+462. Minimum Moves to Equal Array Elements II
+
+Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+
+In one move, you can increment or decrement an element of the array by 1.
+
+Test cases are designed so that the answer will fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 2
+Explanation:
+Only two moves are needed (remember each move increments or decrements one element):
+[1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+Example 2:
+
+Input: nums = [1,10,2,9]
+Output: 16
+ 
+
+Constraints:
+
+n == nums.length
+1 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+*/
+
+//TC O(nlog(n)) SC O(1) soln
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int median = nums[nums.length/2];
+        int ans = 0;
+        for (int num: nums) {
+            ans += Math.abs(median - num);
+        }
+        return ans;
+    }
+}
+
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int l = 0, r = nums.length-1;
+        int ans = 0;
+        while (l < r) {
+            ans += nums[r--] - nums[l++];
+        }
+        return ans;
+    }
+}
+
+//TC O(n^2) SC O(1) soln
+class Solution {
+    public int minMoves2(int[] nums) {
+        long ans = Integer.MAX_VALUE;
+        for (int num: nums) {
+            long diff = 0;
+            for (int n: nums) {
+                diff += Math.abs(num - n);
+            }
+            ans = Math.min(diff, ans);
+        }
+        return (int) ans;
+    }
+}
+
+
