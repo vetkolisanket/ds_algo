6cf1b468c6bbbf524bf15080a2785cf67631dc69
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-07-15 20:20:08 +0530

    Added max area of island example

---
+++ java/dfs/leetcode/max_area_of_island.java
@@ -0,0 +1,58 @@
+/*
+695. Max Area of Island
+
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+
+*/
+
+//TC O(m*n) SC O(1)
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, computeArea(grid, i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
+    
+    private int computeArea(int[][] grid, int i, int j) {
+        if (i < 0 || i == grid.length || j < 0 || j == grid[0].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        int area = 1;
+        area += (computeArea(grid, i-1, j) + computeArea(grid, i, j-1) + computeArea(grid, i+1, j) + computeArea(grid, i, j+1));
+        return area;
+    }
+}
