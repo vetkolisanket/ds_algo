778d8819c45fe6d28a4963fd6628535d72c5cedf
Sanket Vetkoli <vetkolisanket@gmail.com>
2022-08-22 20:55:05 +0530

    Update split_array_into_consecutive_subsequences.java

--- java/arrays/leetcode/split_array_into_consecutive_subsequences.java
+++ java/arrays/leetcode/split_array_into_consecutive_subsequences.java
@@ -41,6 +41,31 @@ Constraints:
 nums is sorted in non-decreasing order.
 */
 
+//Soln after 2nd approach TC O(N) SC O(N)
+class Solution {
+    public boolean isPossible(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap();
+        Map<Integer, Integer> seq = new HashMap();
+        for (int num: nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        for (int num: nums) {
+            if (freq.get(num) == 0) continue;
+            if (seq.getOrDefault(num-1, 0) > 0) {
+                seq.put(num-1, seq.get(num-1)-1);
+                seq.put(num, seq.getOrDefault(num, 0)+1);
+                freq.put(num, freq.get(num)-1);
+            } else if (freq.getOrDefault(num+1, 0) > 0 && freq.getOrDefault(num+2, 0) > 0) {
+                seq.put(num+2, seq.getOrDefault(num+2, 0)+1);
+                freq.put(num, freq.get(num)-1);
+                freq.put(num+1, freq.get(num+1)-1);
+                freq.put(num+2, freq.get(num+2)-1);
+            } else return false;
+        }
+        return true;
+    }
+}
+
 class Solution {
     public boolean isPossible(int[] nums) {
         Map<Integer, Integer> freq = new HashMap();
