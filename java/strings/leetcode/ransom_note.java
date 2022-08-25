38df8128b1580791489073bade84f1a0d54b801c
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-08-25 19:42:12 +0530

    Added ransom note example

---
+++ java/strings/leetcode/ransom_note.java
@@ -0,0 +1,43 @@
+/*
+383. Ransom Note
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ 
+
+Constraints:
+
+1 <= ransomNote.length, magazine.length <= 10^5
+ransomNote and magazine consist of lowercase English letters.
+
+*/
+
+//My soln TC O(m+n) SC O(1) where m and n are the length of ransom note and magazine
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+        for (char c : magazine.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (char c: ransomNote.toCharArray()) {
+            if (freq[c - 'a']-- == 0) return false;
+        }
+        return true;
+    }
+}
