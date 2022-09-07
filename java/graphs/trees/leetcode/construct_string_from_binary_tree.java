7ff2dc65b31e63493a8ef3611a963823f634880d
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-09-07 16:56:19 +0530

    Added construct string from binary tree example

---
+++ java/graphs/trees/leetcode/construct_string_from_binary_tree.java
@@ -0,0 +1,121 @@
+/*
+606. Construct String from Binary Tree
+
+Given the root of a binary tree, construct a string consisting of parenthesis and integers from a binary tree with the preorder traversal way, and return it.
+
+Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4]
+Output: "1(2(4))(3)"
+Explanation: Originally, it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs. And it will be "1(2(4))(3)"
+Example 2:
+
+
+Input: root = [1,2,3,null,4]
+Output: "1(2()(4))(3)"
+Explanation: Almost the same as the first example, except we cannot omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 10^4].
+-1000 <= Node.val <= 1000
+*/
+
+//My soln TC O(n) SC O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    StringBuilder sb = new StringBuilder();
+    
+    public String tree2str(TreeNode root) {
+        sb.append(root.val);
+        if (root.left != null || root.right != null) {
+            if (root.left == null) {
+                sb.append("()");
+            } else {
+                sb.append("(");
+                tree2str(root.left);
+                sb.append(")");
+            }
+            if (root.right != null) {
+                sb.append("(");
+                tree2str(root.right);
+                sb.append(")");
+            }
+        }
+        return sb.toString();
+    }
+}
+
+//A simpler/smaller way having same complexity
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public String tree2str(TreeNode t) {
+        if(t==null)
+            return "";
+        if(t.left==null && t.right==null)
+            return t.val+"";
+        if(t.right==null)
+            return t.val+"("+tree2str(t.left)+")";
+        return t.val+"("+tree2str(t.left)+")("+tree2str(t.right)+")";   
+    }
+}
+
+
+//Soln using stack having same complexity
+
+public class Solution {
+    public String tree2str(TreeNode t) {
+        if (t == null)
+            return "";
+        Stack < TreeNode > stack = new Stack < > ();
+        stack.push(t);
+        Set < TreeNode > visited = new HashSet < > ();
+        StringBuilder s = new StringBuilder();
+        while (!stack.isEmpty()) {
+            t = stack.peek();
+            if (visited.contains(t)) {
+                stack.pop();
+                s.append(")");
+            } else {
+                visited.add(t);
+                s.append("(" + t.val);
+                if (t.left == null && t.right != null)
+                    s.append("()");
+                if (t.right != null)
+                    stack.push(t.right);
+                if (t.left != null)
+                    stack.push(t.left);
+            }
+        }
+        return s.substring(1, s.length() - 1);
+    }
+}
