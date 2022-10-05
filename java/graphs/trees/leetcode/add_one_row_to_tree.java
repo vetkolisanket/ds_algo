89cb4972097ea9fcbd018abb196fe65b93df8f52
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-10-05 13:19:38 +0530

    Added dfs implementation for add one row to tree example

--- java/graphs/trees/leetcode/add_one_row_to_tree.java
+++ java/graphs/trees/leetcode/add_one_row_to_tree.java
@@ -78,3 +78,43 @@ class Solution {
         return root;
     }
 }
+
+//Using DFS TC O(N) SC O(N)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) return new TreeNode(val, root, null);
+        dfs(root, val, depth, 1);
+        return root;
+    }
+    
+    private void dfs(TreeNode node, int val, int depth, int curDepth) {
+        if (node == null) return;
+        if (curDepth == depth) return;
+        if (curDepth == depth-1) {
+            TreeNode left = node.left;
+            TreeNode right = node.right;
+            TreeNode newLeft = new TreeNode(val, left, null);
+            TreeNode newRight = new TreeNode(val, null, right);
+            node.left = newLeft;
+            node.right = newRight;
+        } else {
+            dfs(node.left, val, depth, curDepth+1);
+            dfs(node.right, val, depth, curDepth+1);
+        }
+    }
+}
