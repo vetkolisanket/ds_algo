9dd8cad888e205635658105109eea00f8aba322b
Sanket Vetkoli <vetkolisanket@gmail.com>
2022-10-05 21:52:23 +0530

    Update best_time_to_buy_and_sell_stock_4.java

--- java/dp/leetcode/best_time_to_buy_and_sell_stock_4.java
+++ java/dp/leetcode/best_time_to_buy_and_sell_stock_4.java
@@ -53,3 +53,63 @@ class Solution {
         return profit;
     }
 }
+
+//Another way dp soln TC O(NK) SC O(NK)
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        //If the no. of transactions are more than or equal to half we can take the overall profit as for k transactions we will require atleast 2k days 
+        // (1 day to buy and 1 day to sell)
+        if (2*k >= n) {
+            int res = 0;
+            for (int i=1;i<n;i++) {
+                res += Math.max(0, prices[i] - prices[i-1]);
+            }
+            return res;
+        }
+        // n -> for no. of days, k+1 to account for 0 to k transactions (it is possible max profit is by doing no transactions, prices keep falling everyday)
+        // 2 -> 0 indicates stock is not held, 1 is if stock is held
+        int[][][] dp = new int[n][k+1][2];
+        for (int i=0;i<n;i++) {
+            //Initialising all values to a really low value as on a particular day the values can go below 0 by selling or buying a stock
+            for (int j=0;j<=k;j++) {
+                dp[i][j][0] = -1000000000;
+                dp[i][j][1] = -1000000000;
+            }
+        }
+        //Base case: On 0th day, having done 0 transactions while not holding stock the value will be 0
+        dp[0][0][0] = 0;
+        //Base case: On 0th day, having bought the stock (you can only buy stock on 0th day, can't sell as you have not bought yet), you'd have done
+        //1 transaction, while holding the stock, value will be equal to the price of the stock on 0th day. The value will be negative as you'd have 
+        //bought the stock so you'd have to spend the money to buy the stock
+        dp[0][1][1] = -prices[0];
+        //Having considered the base case we are starting the loop from day 2 (array index 1)
+        for (int i=1;i<n;i++) {
+            //Loop to cover 0 transactions to upto k transactions
+            for (int j=0;j<=k;j++) {
+                //On ith day, having done j transactions while not holding a stock, max value would either be the value on (i-1)th day, having done j
+                //transactions, while not holding any stock or value on (i-1)th day having done j transactions while holding the stock and selling it
+                // on ith day at prices[i] value. (Selling doesn't increase the no. of transactions, only buying does, we are adding prices[i] as 
+                //selling the stock is adding to our profits)
+                dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j][1] + prices[i]);
+                //Since we are performing a transaction, to compute profit we will have to check profits on one transaction before hence the check of j>0
+                if (j > 0) {
+                    //On ith day, having done j transactions while holding a stock, max value would either be the value on (i-1)th day, having done j
+                    //transactions, while holding the stock or value on (i-1)th day having done (j-1) transactions while not holding the stock and buying
+                    //the stock on ith day for prices[i]. (Buying on ith day will increase the no. of transactions, we are subtracting prices[i] as 
+                    //buying the stock will decrease the profit earned up until now)
+                    dp[i][j][1] = Math.max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i]);
+                }
+            }
+        }
+        
+        int res = 0;
+        //Iterate throught the profits on nth day for 0 to upto k transactions to compute the max profit earned. We are only check the not holding part 
+        //(index 0 of 3rd dimension of the array) as buying and holding will always be less profit than holding, why? as buying (index 1) you have to 
+        //spend money, instead not buying (index 0) you'd have more profit
+        for (int i=0;i<=k;i++) {
+            res = Math.max(res, dp[n-1][i][0]);
+        }
+        return res;
+    }
+}
