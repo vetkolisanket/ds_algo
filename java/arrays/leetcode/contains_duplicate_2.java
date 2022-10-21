ce14618b55ef231587e90f8ffcd0af9bbebb238a
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-10-21 11:01:37 +0530

    Added contains duplicate 2 example

---
+++ java/arrays/leetcode/contains_duplicate_2.java
@@ -0,0 +1,42 @@
+/*
+219. Contains Duplicate II
+
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+0 <= k <= 10^5
+*/
+
+//My soln TC O(N) SC O(N)
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i=0;i<nums.length;i++) {
+            int num = nums[i];
+            if (map.containsKey(num)) {
+                if (i - map.get(num) <= k) return true;
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+}
