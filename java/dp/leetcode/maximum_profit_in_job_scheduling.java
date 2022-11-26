52285ff4eb3b4f9984737b1ad113af1cf5fae069
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-11-26 10:05:44 +0530

    Added maximum profit in job scheduling example

---
+++ java/dp/leetcode/maximum_profit_in_job_scheduling.java
@@ -0,0 +1,112 @@
+/*
+1235. Maximum Profit in Job Scheduling
+
+We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+
+You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
+
+If you choose a job that ends at time X you will be able to start another job that starts at time X.
+
+ 
+
+Example 1:
+
+
+
+Input: startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
+Output: 120
+Explanation: The subset chosen is the first and fourth job. 
+Time range [1-3]+[3-6] , we get profit of 120 = 50 + 70.
+Example 2:
+
+
+
+Input: startTime = [1,2,3,4,6], endTime = [3,5,10,6,9], profit = [20,20,100,70,60]
+Output: 150
+Explanation: The subset chosen is the first, fourth and fifth job. 
+Profit obtained 150 = 20 + 70 + 60.
+Example 3:
+
+
+
+Input: startTime = [1,1,1], endTime = [2,3,4], profit = [5,6,4]
+Output: 6
+ 
+
+Constraints:
+
+1 <= startTime.length == endTime.length == profit.length <= 5 * 10^4
+1 <= startTime[i] < endTime[i] <= 10^9
+1 <= profit[i] <= 10^4
+
+*/
+
+//Soln using top down DP + Binary search TC O(Nlog(N)) SC O(N)
+class Solution {
+    // maximum number of jobs are 50000
+    int[] memo = new int[50001];
+    
+    private int findNextJob(int[] startTime, int lastEndingTime) {
+        int start = 0, end = startTime.length - 1, nextIndex = startTime.length;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (startTime[mid] >= lastEndingTime) {
+                nextIndex = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nextIndex;
+    }
+    
+    private int findMaxProfit(List<List<Integer>> jobs, int[] startTime, int n, int position) {
+        // 0 profit if we have already iterated over all the jobs
+        if (position == n) {
+            return 0;
+        }
+        
+        // return result directly if it's calculated 
+        if (memo[position] != -1) {
+            return memo[position];
+        }
+        
+        // nextIndex is the index of next non-conflicting job
+        int nextIndex = findNextJob(startTime, jobs.get(position).get(1));
+        
+        // find the maximum profit of our two options: skipping or scheduling the current job
+        int maxProfit = Math.max(findMaxProfit(jobs, startTime, n, position + 1), 
+                        jobs.get(position).get(2) + findMaxProfit(jobs, startTime, n, nextIndex));
+        
+        // return maximum profit and also store it for future reference (memoization)
+        return memo[position] = maxProfit;
+    }
+    
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        List<List<Integer>> jobs = new ArrayList<>();
+        
+        // marking all values to -1 so that we can differentiate 
+        // if we have already calculated the answer or not
+        Arrays.fill(memo, -1);
+        
+        // storing job's details into one list 
+        // this will help in sorting the jobs while maintaining the other parameters
+        int length = profit.length;
+        for (int i = 0; i < length; i++) {
+            ArrayList<Integer> currJob = new ArrayList<>();
+            currJob.add(startTime[i]);
+            currJob.add(endTime[i]);
+            currJob.add(profit[i]);
+            jobs.add(currJob);
+        }
+        jobs.sort(Comparator.comparingInt(a -> a.get(0)));
+        
+        // binary search will be used in startTime so store it as separate list
+        for (int i = 0; i < length; i++) {
+            startTime[i] = jobs.get(i).get(0);
+        }
+        
+        return findMaxProfit(jobs, startTime, length, 0);
+    }
+}
