e957ccbed03fae7ee03616bfc18df3d5984e7339
Sanket Vetkoli <vetkolisanket@gmail.com>
2022-12-19 11:40:47 +0530

    Update find_if_path_exists_in_graph.java

--- java/graphs/leetcode/find_if_path_exists_in_graph.java
+++ java/graphs/leetcode/find_if_path_exists_in_graph.java
@@ -141,3 +141,31 @@ class Solution {
     }
 }
 
+//Soln using BFS TC O(V+E) SC O(V+E)
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if (source == destination) return true;
+        Queue<Integer> queue = new ArrayDeque();
+        boolean[] visited = new boolean[n];
+        queue.offer(source);
+        visited[source] = true;
+        List<Integer>[] adjList = new ArrayList[n];
+        for (int i=0;i<n;i++) {
+            adjList[i] = new ArrayList();
+        }
+        for (int[] edge: edges) {
+            adjList[edge[0]].add(edge[1]);
+            adjList[edge[1]].add(edge[0]);
+        }
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            for (int p: adjList[v]) {
+                if (p == destination) return true;
+                if (visited[p]) continue;
+                queue.offer(p);
+                visited[p] = true;
+            }
+        }
+        return false;
+    }
+}
