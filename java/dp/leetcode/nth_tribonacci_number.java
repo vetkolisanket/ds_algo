37939ff2145ab7e5d6b52618a66ab74138b97da0
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2023-01-30 16:33:34 +0530

    Added nth tribonacci number problems soln

---
+++ java/dp/leetcode/nth_tribonacci_number.java
@@ -0,0 +1,45 @@
+/*
+1137. N-th Tribonacci Number
+
+The Tribonacci sequence Tn is defined as follows: 
+
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+Given n, return the value of Tn.
+
+ 
+
+Example 1:
+
+Input: n = 4
+Output: 4
+Explanation:
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+Example 2:
+
+Input: n = 25
+Output: 1389537
+ 
+
+Constraints:
+
+0 <= n <= 37
+The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+
+*/
+
+Soln using dp TC O(n) SC O(1)
+class Solution {
+    public int tribonacci(int n) {
+        if (n <= 1) return n;
+        int t0 = 0, t1 = 1, t2 = 1;
+        for (int i=3;i<=n;i++) {
+            int val = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = val;
+        }
+        return t2;
+    }
+}
