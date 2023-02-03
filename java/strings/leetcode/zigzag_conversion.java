4b682195a13b071562d87ed7509c53e339d2304e
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2023-02-03 16:11:17 +0530

    Added another faster approach for zigzag conversion problem

--- java/strings/leetcode/zigzag_conversion.java
+++ java/strings/leetcode/zigzag_conversion.java
@@ -67,3 +67,28 @@ class Solution {
         return sb.toString();
     }
 }
+
+//Another approach using maths TC O(n) SC O(1)
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) return s;
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        int charsInSection = 2*(numRows-1);
+        for (int row = 0;row < numRows;row++) {
+            int index = row;
+            while (index < n) {
+                sb.append(s.charAt(index));
+                if (row != 0 && row != numRows-1) {
+                    int charsInBetween = charsInSection - 2*row;
+                    int secondIndex = index + charsInBetween;
+                    if (secondIndex < n) {
+                        sb.append(s.charAt(secondIndex));
+                    }
+                }
+                index += charsInSection;
+            }
+        }
+        return sb.toString();
+    }
+}
