e484cc47e26cedf2f10f2493f1f4deb178805170
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2023-02-18 08:40:18 +0530

    Added invert binary tree problems soln

---
+++ java/graphs/trees/leetcode/invert_binary_tree.java
@@ -0,0 +1,74 @@
+/*
+226. Invert Binary Tree
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+*/
+
+//Soln using recursion TC O(n) SC O(h) where h is the height of the tree which in worst case can be n
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
+
+//Soln using iteration TC O(n) SC O(n)
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        Queue<TreeNode> queue = new ArrayDeque();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if (node.left != null) queue.offer(node.left);
+            if (node.right != null) queue.offer(node.right);
+        }
+        return root;
+    }
+}
