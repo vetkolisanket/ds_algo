fdd0edac90f8e47015f96006bed3277cf3f4f4b5
Sanket Vetkoli <vetkolisanket@gmail.com>
2023-02-19 07:02:41 +0530

    Update binary_tree_zigzag_level_order_traversal.java

--- java/graphs/trees/leetcode/binary_tree_zigzag_level_order_traversal.java
+++ java/graphs/trees/leetcode/binary_tree_zigzag_level_order_traversal.java
@@ -41,6 +41,31 @@ The number of nodes in the tree is in the range [0, 2000].
  *     }
  * }
  */
+//Soln using BFS TC O(N) SC O(N)
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList();
+        if (root == null) return ans;
+        Queue<TreeNode> queue = new ArrayDeque();
+        boolean isReverse = false;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            if (isReverse) Collections.reverse(list);
+            ans.add(list);
+            isReverse = !isReverse;
+        }
+        return ans;
+    }
+}
+
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> list = new ArrayList();
