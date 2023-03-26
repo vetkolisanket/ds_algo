e62eb2768aa2de21e2910ea9c80145e7663fee41
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2023-03-26 22:48:19 +0530

    Added longest cycle in a graph problems soln

---
+++ java/graphs/leetcode/longest_cycle_in_a_graph.java
@@ -0,0 +1,66 @@
+/*
+2360. Longest Cycle in a Graph
+
+You are given a directed graph of n nodes numbered from 0 to n - 1, where each node has at most one outgoing edge.
+
+The graph is represented with a given 0-indexed array edges of size n, indicating that there is a directed edge from node i to node edges[i]. If there is no outgoing edge from node i, then edges[i] == -1.
+
+Return the length of the longest cycle in the graph. If no cycle exists, return -1.
+
+A cycle is a path that starts and ends at the same node.
+
+ 
+
+Example 1:
+
+
+Input: edges = [3,3,4,2,3]
+Output: 3
+Explanation: The longest cycle in the graph is the cycle: 2 -> 4 -> 3 -> 2.
+The length of this cycle is 3, so 3 is returned.
+Example 2:
+
+
+Input: edges = [2,-1,3,1]
+Output: -1
+Explanation: There are no cycles in this graph.
+ 
+
+Constraints:
+
+n == edges.length
+2 <= n <= 10^5
+-1 <= edges[i] < n
+edges[i] != i
+*/
+
+//Soln using DFS TC O(n) SC O(n)
+class Solution {
+    int answer = -1;
+
+    public void dfs(int node, int[] edges, Map<Integer, Integer> dist, boolean[] visit) {
+        visit[node] = true;
+        int neighbor = edges[node];
+
+        if (neighbor != -1 && !visit[neighbor]) {
+            dist.put(neighbor, dist.get(node) + 1);
+            dfs(neighbor, edges, dist, visit);
+        } else if (neighbor != -1 && dist.containsKey(neighbor)) {
+            answer = Math.max(answer, dist.get(node) - dist.get(neighbor) + 1);
+        }
+    }
+
+    public int longestCycle(int[] edges) {
+        int n = edges.length;
+        boolean[] visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                Map<Integer, Integer> dist = new HashMap<>();
+                dist.put(i, 1);
+                dfs(i, edges, dist, visit);
+            }
+        }
+        return answer;
+    }
+}
