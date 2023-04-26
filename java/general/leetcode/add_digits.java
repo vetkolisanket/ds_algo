3405d670efe82460e8fa96d6fd256d77dab847ba
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2023-04-26 12:09:58 +0530

    Added add digits problems soln

---
+++ java/general/leetcode/add_digits.java
@@ -0,0 +1,52 @@
+/*
+258. Add Digits
+
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1
+ 
+
+Follow up: Could you do it without any loop/recursion in O(1) runtime?
+*/
+
+//Soln using maths
+class Solution {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
+}
+
+//Soln using continuous reduction of num using loop TC O(logN) SC O(1)
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
