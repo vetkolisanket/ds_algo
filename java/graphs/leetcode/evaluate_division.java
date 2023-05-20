e436290b50d523cc5b359bfd91049d15be4a7cf3
Sanket Vetkoli <vetkolisanket@gmail.com>
2023-05-20 19:07:29 +0530

    Update evaluate_division.java

--- java/graphs/leetcode/evaluate_division.java
+++ java/graphs/leetcode/evaluate_division.java
@@ -153,3 +153,70 @@ class Solution {
         return ret;
     }
 }
+
+//Soln using union find TC O((M+N)*log^*(N)) SC O(N)
+class Solution {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        HashMap<String, Pair<String, Double>> gidWeight = new HashMap();
+
+        for (int i=0;i<equations.size(); i++) {
+            List<String> equation = equations.get(i);
+            String dividend = equation.get(0), divisor = equation.get(1);
+            double quotient = values[i];
+            union(gidWeight, dividend, divisor, quotient);
+        }
+
+        double[] results = new double[queries.size()];
+        for (int i=0;i<queries.size();i++) {
+            List<String> query = queries.get(i);
+            String dividend = query.get(0), divisor = query.get(1);
+
+            if (!gidWeight.containsKey(dividend) || !gidWeight.containsKey(divisor)) {
+                results[i] = -1.0D;
+            } else {
+                Pair<String, Double> dividendEntry = find(gidWeight, dividend);
+                Pair<String, Double> divisorEntry = find(gidWeight, divisor);
+
+                String dividendGid = dividendEntry.getKey();
+                String divisorGid = divisorEntry.getKey();
+                double dividendWeight = dividendEntry.getValue();
+                double divisorWeight = divisorEntry.getValue();
+
+                if (!dividendGid.equals(divisorGid)) {
+                    results[i] = -1.0D;
+                } else {
+                    results[i] = dividendWeight/divisorWeight;
+                }
+            }
+        }
+
+        return results;
+    }
+
+    private Pair<String, Double> find(Map<String, Pair<String, Double>> gidWeight, String nodeId) {
+        if (!gidWeight.containsKey(nodeId)) gidWeight.put(nodeId, new Pair(nodeId, 1.0D));
+
+        Pair<String, Double> entry = gidWeight.get(nodeId);
+
+        if (!entry.getKey().equals(nodeId)) {
+            Pair<String, Double> newEntry = find(gidWeight, entry.getKey());
+            gidWeight.put(nodeId, new Pair(newEntry.getKey(), entry.getValue() * newEntry.getValue()));
+        }
+
+        return gidWeight.get(nodeId);
+    }
+
+    private void union(Map<String, Pair<String, Double>> gidWeight, String dividend, String divisor, double value) {
+        Pair<String, Double> dividendEntry = find(gidWeight, dividend);
+        Pair<String, Double> divisorEntry = find(gidWeight, divisor);
+
+        String dividendGid = dividendEntry.getKey();
+        String divisorGid = divisorEntry.getKey();
+        double dividendWeight = dividendEntry.getValue();
+        double divisorWeight = divisorEntry.getValue();
+
+        if (!dividendGid.equals(divisorGid)) {
+            gidWeight.put(dividendGid, new Pair(divisorGid, divisorWeight * value / dividendWeight));
+        }
+    }
+}
