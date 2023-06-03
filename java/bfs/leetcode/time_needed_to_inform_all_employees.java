ea55251c93054375b0953d9c134ee2b006f0c18d
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2023-06-03 15:15:01 +0530

    Added time needed to inform all employees problems soln

---
+++ java/bfs/leetcode/time_needed_to_inform_all_employees.java
@@ -0,0 +1,77 @@
+/*
+1376. Time Needed to Inform All Employees
+
+A company has n employees with a unique ID for each employee from 0 to n - 1. The head of the company is the one with headID.
+
+Each employee has one direct manager given in the manager array where manager[i] is the direct manager of the i-th employee, manager[headID] = -1. Also, it is guaranteed that the subordination relationships have a tree structure.
+
+The head of the company wants to inform all the company employees of an urgent piece of news. He will inform his direct subordinates, and they will inform their subordinates, and so on until all employees know about the urgent news.
+
+The i-th employee needs informTime[i] minutes to inform all of his direct subordinates (i.e., After informTime[i] minutes, all his direct subordinates can start spreading the news).
+
+Return the number of minutes needed to inform all the employees about the urgent news.
+
+ 
+
+Example 1:
+
+Input: n = 1, headID = 0, manager = [-1], informTime = [0]
+Output: 0
+Explanation: The head of the company is the only employee in the company.
+Example 2:
+
+
+Input: n = 6, headID = 2, manager = [2,2,-1,2,2,2], informTime = [0,0,1,0,0,0]
+Output: 1
+Explanation: The head of the company with id = 2 is the direct manager of all the employees in the company and needs 1 minute to inform them all.
+The tree structure of the employees in the company is shown.
+ 
+
+Constraints:
+
+1 <= n <= 105
+0 <= headID < n
+manager.length == n
+0 <= manager[i] < n
+manager[headID] == -1
+informTime.length == n
+0 <= informTime[i] <= 1000
+informTime[i] == 0 if employee i has no subordinates.
+It is guaranteed that all the employees can be informed.
+
+*/
+
+//Soln using bfs TC O(N) SC O(N)
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        int ans = 0;
+        Queue<Cell> queue = new ArrayDeque();
+        List<Integer>[] arr = new ArrayList[n];
+        for (int i=0;i<n;i++) {
+            arr[i] = new ArrayList();
+        }
+        for (int i=0;i<n;i++) {
+            if (manager[i] == -1) continue;
+            arr[manager[i]].add(i);
+        }
+        queue.offer(new Cell(headID, 0));
+        while (!queue.isEmpty()) {
+            Cell cell = queue.poll();
+            for (int neighbour: arr[cell.id]) {
+                int newTime = cell.time + informTime[cell.id];
+                ans = Math.max(ans, newTime);
+                queue.offer(new Cell(neighbour, newTime));
+            }
+        }
+        return ans;
+    }
+
+    class Cell {
+        int id, time;
+
+        public Cell(int id, int time) {
+            this.id = id;
+            this.time = time;
+        }
+    }
+}
