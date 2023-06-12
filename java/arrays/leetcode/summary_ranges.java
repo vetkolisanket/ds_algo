d02c99680cc4ae1753aacc8901ba2d3be36bce91
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2023-06-12 22:29:10 +0530

    Added a more readable soln for summary ranges problem

--- java/arrays/leetcode/summary_ranges.java
+++ java/arrays/leetcode/summary_ranges.java
@@ -40,6 +40,27 @@ All the values of nums are unique.
 nums is sorted in ascending order.
 */
 
+//A more readable soln TC O(N) SC O(1)
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list = new ArrayList();
+        for (int i=0;i<nums.length;i++) {
+            int start = nums[i];
+
+            while (i+1 < nums.length && nums[i+1] == nums[i] + 1) {
+                i++;
+            }
+
+            if (start == nums[i]) {
+                list.add(String.valueOf(start));
+            } else {
+                list.add(start + "->" + nums[i]);
+            }
+        }
+        return list;
+    }
+}
+
 //My soln TC O(N) SC O(1)
 class Solution {
     public List<String> summaryRanges(int[] nums) {
