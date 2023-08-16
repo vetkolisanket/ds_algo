4681f4597bb1433d3c507915c1214254e914f3ec
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2023-08-16 14:35:54 +0530

    Added sliding window maximum problems soln

---
+++ java/deque/leetcode/sliding_window_maximum.java
@@ -0,0 +1,63 @@
+/*
+239. Sliding Window Maximum
+
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10^5
+-10^4 <= nums[i] <= 10^4
+1 <= k <= nums.length
+
+*/
+
+//Soln using deque TC O(N) SC O(K)
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        Deque<Integer> dq = new ArrayDeque();
+        int[] ans = new int[n-k+1];
+        int j = 0;
+        for (int i=0;i<k;i++) {
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
+        ans[j++] = nums[dq.peekFirst()];
+        for (int i=k;i<n;i++) {
+            if (dq.peekFirst() == i-k) {
+                dq.pollFirst();
+            }
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            ans[j++] = nums[dq.peekFirst()];
+        }
+        return ans;
+    }
+}
