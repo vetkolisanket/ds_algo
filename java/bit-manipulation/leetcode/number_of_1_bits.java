f1adc09b06b329b29f7adec65488da155a4e6836
Sanket Vetkoli <vetkolisanket@gmail.com>
2023-11-29 18:27:24 +0530

    Update number_of_1_bits.java

--- java/bit-manipulation/leetcode/number_of_1_bits.java
+++ java/bit-manipulation/leetcode/number_of_1_bits.java
@@ -94,3 +94,16 @@ public class Solution {
         return count;
     }
 }
+
+//Soln from another attempt TC O(1) SC O(1)
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ans = 0;
+        while (n != 0) {
+            ans += (n & 1);
+            n >>>= 1;
+        }
+        return ans;
+    }
+}
