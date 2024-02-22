0fa7522e1f36685002f9739422006c24ceda6897
Sanket Vetkoli <vetkolisanket@gmail.com>
2024-02-22 19:01:26 +0530

    Update find_the_town_judge.java

--- java/arrays/leetcode/find_the_town_judge.java
+++ java/arrays/leetcode/find_the_town_judge.java
@@ -53,3 +53,18 @@ class Solution {
         return -1;
     }
 }
+
+//Soln from another attempt TC O(N+E) SC O(N)
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] inDegree = new int[n+1], outDegree = new int[n+1];
+        for (int[] t: trust) {
+            outDegree[t[0]]++;
+            inDegree[t[1]]++;
+        }
+        for (int i=1;i<=n;i++) {
+            if (inDegree[i] == n-1 && outDegree[i] == 0) return i;
+        }
+        return -1;
+    }
+}
