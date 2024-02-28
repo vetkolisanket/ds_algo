93b769059b0234f07cd7df77fe667d5311ae5139
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2024-02-28 21:31:32 +0530

    Added find bottom left tree value problems soln

---
+++ java/graphs/trees/leetcode/find_bottom_left_tree_value.java
@@ -0,0 +1,63 @@
+/*
+513. Find Bottom Left Tree Value
+
+Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: 1
+Example 2:
+
+
+Input: root = [1,2,3,4,null,5,6,null,null,7]
+Output: 7
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-2^31 <= Node.val <= 2^31 - 1
+
+*/
+
+//Soln using BFS TC O(N) SC O(N)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        int ans = -1;
+        while (!q.isEmpty()) {
+            ans = q.peek().val;
+            int size = q.size();
+            while (size-- > 0) {
+                TreeNode node = q.poll();
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+        }
+        return ans;
+    }
+}
