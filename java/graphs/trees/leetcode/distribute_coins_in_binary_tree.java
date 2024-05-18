19f239c7d64a6ce5e62301dfdd6f6559e344015b
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2024-05-18 08:33:30 +0530

    Added distribute coins in binary tree problems soln

---
+++ java/graphs/trees/leetcode/distribute_coins_in_binary_tree.java
@@ -0,0 +1,70 @@
+/*
+979. Distribute Coins in Binary Tree
+
+You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. There are n coins in total throughout the whole tree.
+
+In one move, we may choose two adjacent nodes and move one coin from one node to another. A move may be from parent to child, or from child to parent.
+
+Return the minimum number of moves required to make every node have exactly one coin.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,0,0]
+Output: 2
+Explanation: From the root of the tree, we move one coin to its left child, and one coin to its right child.
+Example 2:
+
+
+Input: root = [0,3,0]
+Output: 3
+Explanation: From the left child of the root, we move two coins to the root [taking two moves]. Then, we move one coin from the root of the tree to the right child.
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= n <= 100
+0 <= Node.val <= n
+The sum of all Node.val is n.
+
+*/
+
+//Soln using DFS and post-order traversal TC O(N) SC O(N)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private int moves;
+
+    public int distributeCoins(TreeNode root) {
+        moves = 0;
+        dfs(root);
+        return moves;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        moves += Math.abs(left) + Math.abs(right);
+        return node.val - 1 + left + right;
+    }
+}
