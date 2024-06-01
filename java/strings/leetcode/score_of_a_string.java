d05237b6b6e9e4e774874382a439be660e54d14f
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2024-06-01 10:25:18 +0530

    Added score of a string problems soln

---
+++ java/strings/leetcode/score_of_a_string.java
@@ -0,0 +1,49 @@
+/*
+3110. Score of a String
+
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+Return the score of s.
+
+ 
+
+Example 1:
+
+Input: s = "hello"
+
+Output: 13
+
+Explanation:
+
+The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be
+|104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+Example 2:
+
+Input: s = "zaz"
+
+Output: 50
+
+Explanation:
+
+The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+ 
+
+Constraints:
+
+2 <= s.length <= 100
+s consists only of lowercase English letters.
+
+*/
+
+//My soln TC O(N) SC O(1)
+class Solution {
+    public int scoreOfString(String s) {
+        int score = 0, n = s.length();
+        for (int i=1;i<n;i++) {
+            score += Math.abs(s.charAt(i) - s.charAt(i-1));
+        }
+        return score;
+    }
+}
