05485c8b4094c9599bfef9451bc0be8b49744864
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2024-07-02 11:19:41 +0530

    Added soln from another attempt for intersection of two arrays problems soln

--- java/arrays/leetcode/intersection_of_two_arrays_2.java
+++ java/arrays/leetcode/intersection_of_two_arrays_2.java
@@ -78,3 +78,28 @@ class Solution {
         return res;
     }
 }
+
+//Soln from another attempt TC O(M+N) SC O(M)
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num: nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int num: nums2) {
+            if (map.containsKey(num)) {
+                list.add(num);
+                map.put(num, map.get(num)-1);
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                }
+            }
+        }
+        int[] arr = new int[list.size()];
+        for (int i=0;i<arr.length;i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
